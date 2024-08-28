7888a7d7ddede2cefc691a2827d65664d4576323
riyasiddabattula <riyasiddabattula@localhost>
2024-08-28 14:07:21 -0500

    Riya's Trial Message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -5,5 +5,7 @@ public class Message {
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");	
 		System.out.println("Hey it's Celine.");	
+		System.out.println("Hi it's Riya!");	
+
 	}
 }
