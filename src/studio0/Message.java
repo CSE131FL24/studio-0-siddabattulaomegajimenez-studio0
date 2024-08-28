8c523fa101467d1b14ba22e079a974a28a745875
mysbi <mysbi@LAPTOP-T4TNM0R0.wusm-wifi.wucon.wustl.edu>
2024-08-28 14:08:24 -0500

    Sarahhhh

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -5,7 +5,8 @@ public class Message {
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");	
 		System.out.println("Hey it's Celine.");	
-		System.out.println("Hi it's Riya!");	
+		System.out.println("Hi it's Riya!");
+		System.out.println("Hiii it's Sarah :)");
 
 	}
 }
