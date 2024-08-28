5a2c5751e95c715402b0dfef9ac2a13e6c78d84d
celin <celin@Celine_Machine.dhcp.wustl.edu>
2024-08-28 14:05:26 -0500

    Celine Group Message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,8 +3,7 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("Say hi to your group mates!");
-		System.out.println("Hiii it's Sarah");
-		
+		System.out.println("Say hi to your group mates!");	
+		System.out.println("Hey it's Celine.");	
 	}
 }
